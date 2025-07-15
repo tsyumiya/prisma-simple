b218f55a9e8497e747e2777f1d5e401b47076ba3
TY <thales.y@gmail.com>
2025-07-15 01:05:26 -0300

    remove cache

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -3,26 +3,26 @@ import EditPost from "@/app/components/edit/editForm"
 import { notFound } from "next/navigation"
 import { cache } from "react"
 
-const getCachedPost = cache(async (id: string) => {
-  return await prisma.post.findUnique({
-    where: {
-      id
-    }
-  })
-})
+// const getCachedPost = cache(async (id: string) => {
+//   return await prisma.post.findUnique({
+//     where: {
+//       id
+//     }
+//   })
+// })
 
 export default async function Post({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params
 
   console.log("Post ID:", id)
 
-  const post = await getCachedPost(id)
+  // const post = await getCachedPost(id)
 
-  // const post = await prisma.post.findUnique({
-  //   where: {
-  //     id
-  //   }
-  // })
+  const post = await prisma.post.findUnique({
+    where: {
+      id
+    }
+  })
 
   console.log("Post:", post)
 
